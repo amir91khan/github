06415c9e856cb02f53f25b0e8dc2cd11404320e4
aamir <aamir@khan>
2018-06-13 16:27:10 -0700

    Aamir-change1

--- Demogit/src/com/nt/Demo1.java
+++ Demogit/src/com/nt/Demo1.java
@@ -3,7 +3,8 @@ package com.nt;
 public class Demo1 {
 
 	public static void main(String[] args) {
-System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello1");
 
 	}
 
