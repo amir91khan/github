b73f9fe4d717e1b97b7328d717115e6b1b8d3dba
hp <hp@hp-PC>
2018-06-14 16:35:35 +0530

    commit3

--- Demogit/src/com/nt/Demo1.java
+++ Demogit/src/com/nt/Demo1.java
@@ -3,11 +3,11 @@ package com.nt;
 public class Demo1 {
 
 	public static void main(String[] args) {
-System.out.println("hello");
-//change by viren 
-System.out.println("viren");
- int i = 10;
- System.out.println(i);
+		System.out.println("hello");
+		// change by viren
+		System.out.println("viren");
+		int i = 10;
+		System.out.println(i);
 	}
 
 }
