9cd8bdfdb7991a5369a02406314b87d599299a63
DILKHUSH JHA <DILKHUSH JHA@Satish>
2018-06-14 15:58:35 +0530

    Devloper1-change-1st time

--- GitProj1/src/com/nit/wish/Wish.java
+++ GitProj1/src/com/nit/wish/Wish.java
@@ -3,6 +3,7 @@ package com.nit.wish;
 public class Wish {
 	public static void main(String[] args) {
 		System.out.println("welcome to git hub");
+		System.out.println("Change from Devloper 1 system");
 	}
 
 }
